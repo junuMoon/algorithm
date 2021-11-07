9c76fe88de28c25d5385a92312bf62c4b9fae112
Junu_Moon <francomoon7@chorock.co.kr>
2021-11-07 21:37:21 +0900

    Finished baekjoon 2941

--- python/baekjoon/b2941.py
+++ python/baekjoon/b2941.py
@@ -11,24 +11,45 @@ condition_dict = {
 }
 
 
-smp = 'ljes=njakddz=abnljj----dz===='
+smp = 'dz====dz=nljjj'
 total = 0
+
 while smp:
     check_lst = condition_dict.get(smp[0])
     if not check_lst:
-        smp = smp[1:]
-        total += 1
-    else:
-        checked = False
-        for check in check_lst:
-            if smp[0: len(check)] == check:
-                print(f"special char: {smp[0:len(check)]}")
-                smp = smp[len(check):]
-                total += 1
-                checked = True
-        if not checked:
+        if smp[0] not in ['-', '=']:
             smp = smp[1:]
             total += 1
+        else:
+            smp = smp[1:]
+    else:
+        for check in check_lst:
+            if smp[:len(check)] == check:
+                print(check)
+                smp = smp[len(check):]
+                total += 1
+                break
+        else:
+            smp = smp[1:]
+            total += 1
+
+
+# while smp:
+#     check_lst = condition_dict.get(smp[0])
+#     if not check_lst:
+#         smp = smp[1:]
+#         total += 1
+#     else:
+#         checked = False
+#         for check in check_lst:
+#             if smp[0: len(check)] == check:
+#                 print(f"special char: {smp[0:len(check)]}")
+#                 smp = smp[len(check):]
+#                 total += 1
+#                 checked = True
+#         if not checked:
+#             smp = smp[1:]
+#             total += 1
 
 print(total)
     
